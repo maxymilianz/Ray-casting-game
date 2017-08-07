3366d038267f2bf9005a19af8690a6229d6b58a2
Maksymilian Zawartko <289528@uwr.edu.pl>
2017-08-07 11:10:17 +0200

    Removing unnecessary import

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -11,7 +11,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.LinkedList;
